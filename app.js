2580e4d76806b738a467c2c45b2840d20dfc25e1
Abdul Samad <abdulsamamd9004607@gmail.com>
2021-06-12 12:55:53 +0500

    backspace btn add

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
-function val(num){
-    document.getElementById('display').value += num
+var input = document.getElementById('display')
+
+function val(num) {
+   
+    input.value += num
+    // if (num === "+" || num === "-" || num === "*" || num === "/" || num === "%") {
+    //     var arr = ["+", "-", "*", "/", "%"]
+
+    //     for (var i = 0; i < arr.length; i++) {
+    
+    //         if (input.value.charAt(input.value.length - 1) === "+") {
+    //             input.value.slice(0, input.value.length - 3) +  input.value.slice(input.value.length - 2, input.value.length)
+    //             // input.value.slice(input.value.length-2,input.value.length) = input.value.slice(input.value.length-1,input.value.length)
+    //         }else{ input.value += num}
+    // //     }
+    // }
+
+  
 }
 
-function calc(){
-    if(document.getElementById('display').value === ""){
-        document.getElementById('display').value = ""
-    }else{
- document.getElementById('display').value = eval(document.getElementById('display').value)
-}
+function calc() {
+    if (input.value === "") {
+        input.value = ""
+    } else {
+        input.value = eval(input.value)
+    }
 }
 
-function clr(){
-    document.getElementById('display').value = ''
+function clr() {
+    input.value = ''
+}
+
+function back() {
+    input.value = input.value.slice(0, input.value.length - 1)
 }
