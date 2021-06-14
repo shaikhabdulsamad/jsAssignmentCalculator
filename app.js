ccf581bd066f402f85b5eafa15b621a779ddefc9
Abdul Samad <abdulsamamd9004607@gmail.com>
2021-06-14 13:18:48 +0500

    some functions add update ui

--- app.js
+++ app.js
@@ -1,21 +1,16 @@
 var input = document.getElementById('display')
 
 function val(num) {
-   
-    input.value += num
-    // if (num === "+" || num === "-" || num === "*" || num === "/" || num === "%") {
-    //     var arr = ["+", "-", "*", "/", "%"]
 
-    //     for (var i = 0; i < arr.length; i++) {
-    
-    //         if (input.value.charAt(input.value.length - 1) === "+") {
-    //             input.value.slice(0, input.value.length - 3) +  input.value.slice(input.value.length - 2, input.value.length)
-    //             // input.value.slice(input.value.length-2,input.value.length) = input.value.slice(input.value.length-1,input.value.length)
-    //         }else{ input.value += num}
-    // //     }
-    // }
+    if (input.value.match(/[-,+,*,/,%]$/) && num.match(/[-,+,*,/,%]/)) {
 
-  
+        input.value = input.value.slice(0,input.value.length -1) + num;
+        
+    }
+
+    else { input.value += num }
+
+
 }
 
 function calc() {
