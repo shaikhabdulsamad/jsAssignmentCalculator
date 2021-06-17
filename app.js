24c97d985acd45071c2b3cbd656e3e4f7f24f48c
Abdul Samad <abdulsamamd9004607@gmail.com>
2021-06-17 14:19:55 +0500

    code update

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ var input = document.getElementById('display')
 
 function val(num) {
 
-    if (input.value.match(/[-,+,*,/,%]$/) && num.match(/[-,+,*,/,%]/)) {
+    var lastVal = input.value.slice(input.value.length - 1);
 
-        input.value = input.value.slice(0,input.value.length -1) + num;
-        
+    var opr = ["+", "-", "*", "/"];
+
+    if (opr.indexOf(lastVal) !== -1 && opr.indexOf(num) !== -1) {
+        input.value = input.value.slice(0, input.value.length - 1) + num
     }
-
     else { input.value += num }
-
 
 }
 
